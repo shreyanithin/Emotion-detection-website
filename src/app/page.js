180c8a284f87cb137ac7e0b8db61e0917780678e
shreyanithin <shreyanithinr@gmail.com>
2025-02-24 00:53:37 +0530

    landing page done

--- src/app/page.js
+++ src/app/page.js
@@ -1,32 +1,33 @@
 export default function Home() {
   return (
-    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6">
-      <h1 className="text-4xl font-bold text-gray-800 mb-4">
-        Emotion Detection System
+    <div className="flex flex-col items-center justify-center min-h-screen bg-gray-100 p-6 bg-hero-pattern bg-cover bg-center h-screen flex items-center ">
+      <div className="bg-white/5 backdrop-blur-lg rounded-lg shadow-lg p-8 max-w-md text-center">
+      <h1 className="text-4xl font-bold text-white-800 mb-4">
+        SoulWave
       </h1>
-      <p className="text-lg text-gray-600 text-center mb-6">
-        Analyze emotions from audio and video input in real-time.
-      </p>
-
-      {/* Feature Section */}
-      <div className="bg-white shadow-lg rounded-2xl p-6 max-w-2xl text-center mb-6">
-        <h2 className="text-2xl font-semibold text-gray-700 mb-2">Features</h2>
-        <ul className="text-gray-600">
-          <li>🎥 Live Camera Emotion Detection</li>
-          <li>🎙️ Audio Emotion Analysis</li>
-          <li>🤖 AI-based Chat & Recommendations</li>
-        </ul>
-      </div>
+      <p className="text-lg text-gray-400 text-center mb-6">
+        Beyond Words – Let AI Read Your Heart.
+      </p> 
 
       {/* Login & Signup Buttons */}
-      <div className="flex gap-4">
+      <div className="flex gap-10 flex items-center justify-center" >
         <a href="/login" className="px-6 py-3 bg-blue-500 text-white rounded-lg shadow-md hover:bg-blue-600 transition">
           Login
         </a>
         <a href="/signup" className="px-6 py-3 bg-green-500 text-white rounded-lg shadow-md hover:bg-green-600 transition">
           Sign Up
         </a>
-      </div>
+        </div>
+      </div> <br></br>
+      <footer className="mt-10 text-white text-center">
+  <p>&copy; 2025 SoulWave. All rights reserved.</p>
+  <p className="mt-2">
+    <a href="#" className="underline">Privacy Policy</a> • 
+    <a href="#" className="underline">Terms of Service</a>
+  </p>
+</footer>
+
     </div>
+    
   );
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -11,6 +11,9 @@ export default {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      backgroundImage: {
+        'hero-pattern': "url('/bg3.avif')", 
+      },
     },
   },
   plugins: [],
